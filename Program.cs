b13288e81cef2ea8963ff24e8d6470fd7b453d59
geetha priyakumar <geetha.priyakumar@byjusteachers.com>
2022-11-30 21:25:37 +0530

    23

--- Program.cs
+++ Program.cs
@@ -5,35 +5,40 @@
         static void Main(string[] args)
         {
         User me = new User();
-       try{
-       while(true){
+        try{
+            while(true){
 
-        Console.WriteLine("Choose 1 to View data or 2 to Update data and 0 to exit");
-        int option=Convert.ToInt32(Console.ReadLine());
-        if(option==1){
-                 me.View();
-        
-                continue;
-        }
-        else if(option==2){
-                me.Update();  
-                me.View();
-               
-                continue;
-        }
-        else if(option==0){
-            System.Environment.Exit(0);  
-        }
-        else {
-       
-        Console.WriteLine("Choose correct option");
-        continue;
-        }
-       }
+                Console.WriteLine("Choose 1 to View data or 2 to Update data and 0 to exit");
+                
+                int option=Convert.ToInt32(Console.ReadLine());
+
+                if(option==1){
+                        
+                        me.View();
+                
+                        continue;
+                }
+                else if(option==2){
+                    
+                        me.Update();  
+                        me.View();
+                    
+                        continue;
+                }
+                else if(option==0){
+                    System.Environment.Exit(0);  
+                }
+                else {
+            
+                    Console.WriteLine("Choose correct option");
+                    continue;
+                }
+            }
        }
    catch (Exception e)
         {
             Console.WriteLine("{0} Exception caught.", e);
+            System.Environment.Exit(0);  
         }
       
         }
--- user.cs
+++ user.cs
@@ -4,29 +4,51 @@ namespace Class2Hw
     {
        
         public User(){
+            try{
+                Console.WriteLine("Enter the Name");
+                Name=Console.ReadLine();
 
-            Console.WriteLine("Enter the Name");
-            Name=Console.ReadLine();
-            Console.WriteLine("Enter the Age");
-            Age=Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the Mobile No.");
-            MobileNo=long.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the Age");
+                Age=Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter the Mobile No.");
+                MobileNo=long.Parse(Console.ReadLine());
+            } 
+            catch (Exception e)
+            {
+                Console.WriteLine("{0} Exception caught.", e);
+                System.Environment.Exit(0);  
+            }
 
         }
         public override void View(){
-            Console.WriteLine(Name);
-            Console.WriteLine(Age);
-            Console.WriteLine(MobileNo);
+            Console.WriteLine("Name : "+Convert.ToString(Name));
+
+            Console.WriteLine("Age : "+ Convert.ToString(Age));
+
+            Console.WriteLine("MobileNo: "+Convert.ToString(MobileNo));
 
         }
         public override void Update(){
-            Console.WriteLine("Enter the Name");
-            Name=Console.ReadLine();
-            Console.WriteLine("Enter the Age");
-            Age=Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the Mobile No.");
-            MobileNo=long.Parse(Console.ReadLine());
-             Console.WriteLine("Contact Updated Sucessfully");
+
+            try{
+            
+                Console.WriteLine("Enter the Name");
+                Name=Console.ReadLine();
+
+                Console.WriteLine("Enter the Age");
+                Age=Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter the Mobile No.");
+                MobileNo=long.Parse(Console.ReadLine());
+
+                Console.WriteLine("Contact Updated Sucessfully");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("{0} Exception caught.", e);
+                System.Environment.Exit(0);  
+            }
         }
     }
 
